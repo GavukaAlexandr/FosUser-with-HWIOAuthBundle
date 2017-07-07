50d46dde6bfc927bc103c84777746ddac3f580dd
Alexandr Gavuka <alexandrgavuka@gmail.com>
2017-07-08 00:28:40 +0300

    added form for upload files.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,54 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\FormUploadFilesType as Form;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    public function uploadAction(Request $request)
+    {
+        $form = $this->createForm(Form::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $folderName = $form['folder_name']->getData();
+            $files = $form['upload_file']->getData();
+
+            foreach ($files as $file) {
+                $fileName = $file->getClientOriginalName();
+
+//                $fs = new Filesystem();
+//                if (!$fs->exists($this->getParameter('app.uploads_music_dir') . "/$folderName")) {
+//                    $fs->mkdir($folderName, 0777);
+
+//                }
+
+                if ($file->move(
+                    $this->getParameter('app.uploads_music_dir') . "/$folderName",
+                    $fileName)) {
+
+                } else {
+                    echo "<pre>" . var_dump('files not saved') . "</pre>";exit;
+                }
+            }
+        }
+
+        return $this->render(
+            'AppBundle:Default:upload.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 }
---
+++ src/AppBundle/Form/FormUploadFilesType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class FormSearch
+ * @package BlogBundle\Forms
+ */
+class FormUploadFilesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'folder_name'
+            )
+            ->add(
+                'upload_file',
+                FileType::class,
+                [
+                    'multiple' => true,
+                ]
+            );
+    }
+}
