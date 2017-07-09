f7c22bbaa6d10bb77ebca925ae55869275a27b0c
Alexandr Gavuka <alexandrgavuka@gmail.com>
2017-07-09 15:13:39 +0300

    fixed submit button

--- src/AppBundle/Form/FormUploadFilesType.php
+++ src/AppBundle/Form/FormUploadFilesType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
@@ -28,6 +29,12 @@ class FormUploadFilesType extends AbstractType
                 [
                     'multiple' => true,
                 ]
-            );
+            )
+            ->add('upload',
+                SubmitType::class,
+                [
+                    'label' => 'upload'
+                ]
+                );
     }
 }
